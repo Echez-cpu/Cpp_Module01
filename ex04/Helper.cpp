912a9965d2877fcce870620e98e4e7e29dcf79f6
Patrick Echezona Okpalaeze <pokpalae@c3r5s1.42wolfsburg.de>
2024-11-26 16:04:24 +0100

    Error management solidified

--- ex04/Helper.cpp
+++ ex04/Helper.cpp
@@ -6,7 +6,7 @@
 /*   By: pokpalae <pokpalae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 16:28:55 by pokpalae          #+#    #+#             */
-/*   Updated: 2024/11/22 16:34:35 by pokpalae         ###   ########.fr       */
+/*   Updated: 2024/11/26 16:02:40 by pokpalae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,18 @@ void replaceInFile(const str &filename, const str &s1, const str &s2)
         return;
     }
 
+    if (infile.peek() == std::ifstream::traits_type::eof()) 
+    {
+        std::cerr << "\033[33mWarning\033[0m: The input file is empty. No replacements were made." << std::endl;
+        infile.close();
+        return;
+    }
+
     std::ofstream outfile((filename + ".replace").c_str());
     if (!outfile.is_open())
 	{
         std::cerr << "Error: Could not create output file." << std::endl;
+        infile.close();
         return;
     }
 
@@ -45,7 +53,18 @@ void replaceInFile(const str &filename, const str &s1, const str &s2)
         newLine.append(line, pos);
 
         outfile << newLine << std::endl;
+        
+        if (infile.bad())
+        {
+            std::cerr << "Error: Failed to read the input file due to an I/O error." << std::endl;
+            infile.close();
+            outfile.close();
+            return;
+        }
     }
 
-    std::cout << "File created successfully. New file: " << filename << ".replace" << std::endl;
+    infile.close();
+    outfile.close();
+
+    std::cout << "\033[32mFile created successfully. New file\033[0m: " << filename << ".replace" << std::endl;
 }
--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pokpalae <pokpalae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 16:45:31 by pokpalae          #+#    #+#             */
-/*   Updated: 2024/11/22 22:04:56 by pokpalae         ###   ########.fr       */
+/*   Updated: 2024/11/26 15:49:34 by pokpalae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,12 @@ int main(int argc, char **argv)
     str filename = argv[1];
     str s1 = argv[2];
     str s2 = argv[3];
+
+    if (s1.empty()) 
+    {
+         std::cerr << "\033[31mError\033[0m: The string to replace (s1) cannot be empty." << std::endl;
+        return 1;
+    }
 
     replaceInFile(filename, s1, s2);
 
